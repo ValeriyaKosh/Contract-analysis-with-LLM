4b2a7500642986c6e8f8185eee75d9657ac32c1f
ValeriyaKosh <166523530+ValeriyaKosh@users.noreply.github.com>
2024-04-19 13:43:48 +0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -7,25 +7,7 @@
 #SBATCH --error=trial1.%J.err
 
 # Choose the model that we want to use
-module load model-llama2/13b-chat
-
-????????? # Choose the llama.cpp model quantization we want to use
-????????? module load model-llama.cpp/q4_1-2023-08-28
-
-# Get the path to model weights
-echo $MODEL_WEIGHTS
+module load model-huggingface/all
 
   # run python
-srun python LLM-pdf-reader/code.py
-
-
-
-
-
-#run on Login node:
- # setup environment
-module load miniconda 
-conda env create -f LLM-pdf-reader/env.yml -p ./LLMTrial1 #(only once)
-source activate ./LLMTrial1
-
-sbatch code.sh
+srun python Contract-analysis-with-LLM/code.py
