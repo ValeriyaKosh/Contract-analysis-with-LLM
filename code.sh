28ad9f056a6c257a3711ed628c404b13b80b8574
ValeriyaKosh <166523530+ValeriyaKosh@users.noreply.github.com>
2024-04-19 13:31:55 +0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-  #SBATCH --time=00:25:00
-  #SBATCH --cpus_per_task=4
-  #SBATCH --mem=20GB
-  #SBATCH --gres=gpu:1
-  #SBATCH --output=trial1.%J.out
-  #SBATCH --error=trial1.%J.err
-
-
+#SBATCH --time=00:25:00
+#SBATCH --cpus_per_task=4
+#SBATCH --mem=20GB
+#SBATCH --gres=gpu:1
+#SBATCH --output=trial1.%J.out
+#SBATCH --error=trial1.%J.err
 
 # Choose the model that we want to use
 module load model-llama2/13b-chat
@@ -17,10 +15,17 @@ module load model-llama2/13b-chat
 # Get the path to model weights
 echo $MODEL_WEIGHTS
 
-
-  # setup environment
-?????????? conda env create -f LLM-pdf-reader/env.yml -p ./myenv
-#### should I use .yaml or .yml ?
-
   # run python
 srun python LLM-pdf-reader/code.py
+
+
+
+
+
+#run on Login node:
+ # setup environment
+module load miniconda 
+conda env create -f LLM-pdf-reader/env.yml -p ./LLMTrial1 #(only once)
+source activate ./LLMTrial1
+
+sbatch code.sh
